b020c0ac65b2abf5db467ef337a272cb7a745309
Yogi_Patel <yogiranchhod@gmail.com>
2022-10-25 21:12:56 -0700

    Temporary commit. Working on the list component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,7 +10,9 @@ class Home extends Component
             username: this.props.AppState.username,
             signedIn: this.props.AppState.signedIn,
             filter: "all",
-            searchField: ''
+            searchField: '',
+            add: false,
+            update: false
         }
     }
 
@@ -32,23 +34,43 @@ class Home extends Component
         
         return (
             <article id = 'home_container' className=" center br2 mb5  shadow-5-ns ">
+                
+                
+                {/* Start of left side home div */}
                 <div className = 'home-div fl w-75 pa2 lightest-gray'>
-                    <div id = "main" className = "shadow-2-ns h-10 br4 ma3 ml3 ">
+                    <div id = "main" className = "shadow-2-ns h-10 br4 ma3 ml3 bg-white">
                         <div id = "vertical-center" className="horizontal-align">
                             <img  src = {logo} alt="logo" /> 
                             <h2  className="v-mid pl3" >Application Tracker</h2> 
-                            <button class="no-underline near-white bg-animate bg-near-black hover-bg-gray inline-flex items-center ma2 tc br-pill pa2 ml3 button_without_style">
-                                <svg class="dib h2" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" 
-                                stroke-miterlimit="1.414">
-                                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" fill-rule="nonzero"/></svg>
-                                <span class="f5 ml3 pr2">Add job</span>
+                            <button className="no-underline near-white bg-animate home-color hover-bg-light-green  inline-flex items-center ma2 tc br-pill pa2 ml3 button_without_style">
+                                <svg className="dib h2" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fillRule="evenodd" clipRule="evenodd" strokeLinejoin="round" 
+                                strokeMiterlimit="1.414">
+                                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" fillRule="nonzero"/></svg>
+                                <span className="f5 ml3 pr2">Add job</span>
                             </button>
-                            
-                        </div>
-                        
+                        </div >
                     </div>
 
+                    {/* Start of Scrollable list of all the jobs that have been added */}
+                    <div className="scrollable no-scrollbar h-85 mt5">
+                        {/* Start of singular div for displaying a list */}
+                        <div  className = "relative-main shadow-2-ns h3 br4 ma3 ml3 bg-white">
+                            <div id = "vertical-center" className="horizontal-align"> 
+                                <h3 className="v-mid pl3" >Job</h3>
+                                <p className="v-mid pl1" >@ company</p>
+                                <a className="pl4" href = "www.google.com" target="_blank">google.com</a>
+                            </div >
+                        </div>
+                        {/* End of singular div for displaying a list */}
+                        
+                    </div>
+                    {/* End of Scrollable list of all the jobs that have been added */}
+
                 </div>
+                {/* End of left side home div */}
+
+
+                {/* Start of sidebar on the right of the home div */}
                 <div className = " home-div shadow-left fl w-25 pa0 ">
                     <div id = "main" className ="home-color " ><h1 id = "bottom" className="f3 f2-m f1-l fw2 black-90 mt0 tl ml3 mb2">Menu</h1></div>
                     <div className="pa0 mt4">
@@ -74,6 +96,7 @@ class Home extends Component
                         </ul>
                     </div>
                 </div>
+                {/* End of sidebar on the right of the home div */}
 
             </article>
             
